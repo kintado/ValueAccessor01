abe7738dbf167c2d6fa07d42a4d211ab139959c3
Maurizio Postiglione <maurizio.postiglione@gmail.com>
2025-06-30 10:03:48 +0200

    ValueAccessor 01

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Ratingstar } from './components/cva/ratingstar/ratingstar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, Ratingstar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App {  
   protected title = 'cvaexaple01';
+  myform = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    cognome: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    anni: new FormControl('', [Validators.required, Validators.min(18), Validators.max(99)]),
+    rating: new FormControl(0, [Validators.required])
+  });
+
+  setta(n : number){
+    this.myform.get ('rating')?.setValue(n);
+  }
+
+  testSetta()
+  {
+    this.setta(3);
+  }
+
 }
---
+++ src/app/components/cva/ratingstar/ratingstar.ts
@@ -0,0 +1,60 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'inputrating',
+  imports: [CommonModule],
+  standalone: true,
+  template: `
+  &nbsp;
+  <span *ngFor="let i of [0, 0, 0, 0, 0]; let index = index" class="star" [class.filled]="index < value" (click)="rate(index + 1)" >★</span>
+  &nbsp;
+  `,
+  styles: [`
+  .star {
+    font-size: 2rem;
+    color: lightgray;
+    cursor: pointer;
+  }
+  .star.filled {
+    color: gold;
+  }
+  `],
+  
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: Ratingstar,
+      multi: true
+    }
+  ]
+})
+export class Ratingstar implements ControlValueAccessor {
+    value = 0;
+
+
+    private onChange: (value: number) => void = () => {};
+    private onTouched: () => void = () => {};
+
+    rate(number: number): void {
+      this.value = number;
+      this.onChange(number);
+      this.onTouched();
+    }
+
+    writeValue(v: number): void {
+      this.value = v
+    
+    }
+
+    registerOnChange(fn: any): void {
+      this.onChange = fn;
+    }
+    registerOnTouched(fn: any): void {
+      this.onTouched = fn;
+    }
+
+ 
+}
+
